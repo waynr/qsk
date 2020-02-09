5864ea3081ea8abdac83863287affc4e4df8d1ca
Wayne Warren <wayne.warren.s@gmail.com>
2020-02-09 20:45:00 +0000

    implement initial keycode scanner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use evdev_rs::enums;
+use evdev_rs::Device;
+use std::error;
+use std::fs::File;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let f = File::open("/dev/input/by-path/platform-i8042-serio-0-event-kbd")?;
+
+    let mut d = Device::new().unwrap();
+    d.set_fd(f)?;
+
+    loop {
+        let a = d.next_event(evdev_rs::ReadFlag::NORMAL | evdev_rs::ReadFlag::BLOCKING)?;
+
+        match (&a.1.event_type, &a.1.event_code) {
+            (enums::EventType::EV_KEY, ec) => match ec {
+                enums::EventCode::EV_KEY(enums::EV_KEY::KEY_PAUSE) => break,
+                _ => println!("{} {}", ec, &a.1.value),
+            },
+            (_, _) => (),
+        }
+    }
+    Ok(())
 }
