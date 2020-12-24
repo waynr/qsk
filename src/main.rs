11b328e43491b54447c6393c2115bad54e1689c0
Wayne Warren <wayne.warren.s@gmail.com>
2020-12-23 19:20:46 -0600

    qsk: remove unnecessary sleep from main

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,6 @@ async fn doit() -> Result<(), Box<dyn error::Error>> {
     let matches = get_clap_app()?;
     let input_events_file = value_t!(matches, "device-file", PathBuf)?;
 
-    std::thread::sleep(std::time::Duration::from_millis(1000));
     let myd = Device::from_path(input_events_file)?;
     let ui = myd.new_uinput_device()?;
 
