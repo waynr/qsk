6f8f766b4220b7bc6fddf69236ac73c45491f8c2
Wayne Warren <wayne.warren.s@gmail.com>
2022-01-28 20:54:26 -0700

    qsk: add timeout before starting remap engine

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::error;
+use std::thread::sleep;
+use std::time::Duration;
 
 use async_std::task;
 use async_compat::Compat;
@@ -33,6 +35,12 @@ impl From<LAYERS> for usize {
 
 async fn remap(matches: &ArgMatches) -> Result<(), Box<dyn error::Error>> {
     let input_events_file = matches.value_of_t("device-file")?;
+
+    // give input source events time to finish before grabbing. this is necessary if the keyboard
+    // being remapped is the one where "enter" is pressed on the command line to call `qsk` in the
+    // shell
+    //
+    sleep(Duration::from_millis(300));
 
     let myd = Device::from_path(input_events_file)?;
     //let ui = myd.new_uinput_device(String::from("qsk-keyboard"))?;
