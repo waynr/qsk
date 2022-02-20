388ad2c158137620d0a654a160d10c571b8741de
Wayne Warren <wayne.warren.s@gmail.com>
2022-02-19 21:33:32 -0700

    qsk-types: remove default noop test case

--- qsk-types/src/lib.rs
+++ qsk-types/src/lib.rs
@@ -8,12 +8,3 @@ pub use layers::*;
 pub use layer_composer::*;
 pub use events::*;
 pub use control_code::*;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
