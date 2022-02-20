ba22544a94716132f54e13817ae9b0184263bcfe
Wayne Warren <wayne.warren.s@gmail.com>
2022-02-19 21:49:06 -0700

    qsk: remove noop test modules

--- src/device/lib.rs
+++ src/device/lib.rs
@@ -1,10 +1,2 @@
 pub mod linux;
 pub mod linux_evdev;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
--- src/engine.rs
+++ src/engine.rs
@@ -107,11 +107,3 @@ impl QSKEngine {
         Ok(())
     }
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
