42b62036a49e8484efb28ced8ee94ad35adfb224
Wayne Warren <wayne.warren.s@gmail.com>
2022-02-27 21:56:52 -0700

    qsk-macros: get keymap definition macro working
    Finally! This PR implements:
    
    * "lowering" the `LayerComposer` domain model into the "intermediate
      representation" struct called `Ir`, which is more or less just a
      struct of `syn`/`proc_macro2` types that can be interpolated into
      `quote!` during codegen.
    * generating code from the lowered `Ir` into a valid Rust TokenStream
    
    One notable thing that is left to be done from here is to eliminate the
    use of `hashmap!`, which is problematic because it forces the macro
    calling context to depend on the hashmap crate in addition to
    `qsk-macros`.

--- qsk-macros/src/codegen.rs
+++ qsk-macros/src/codegen.rs
@@ -1,7 +1,59 @@
 use proc_macro2::TokenStream;
+use quote::quote;
 
-use crate::lower::Ir;
+use crate::lower;
 
-pub fn codegen(ir: Ir) -> TokenStream {
-    TokenStream::new()
+pub fn codegen(ir: lower::Ir) -> TokenStream {
+    let layers_quoted: Vec<TokenStream> = ir.layers.into_iter()
+        .map(|layer| {
+            let name = layer.name;
+            let active = layer.active;
+            let maps: Vec<TokenStream> = layer.maps.into_iter()
+                .map(|map| {
+                    let key_path = map.key;
+                    map.control_code
+                        .iter()
+                        .map(|cc| {
+                            match cc {
+                                lower::ControlCode::Key(control_code_path) => {
+                                    quote!(
+                                        #key_path => vec![qsk_types::ControlCode::KeyMap(#control_code_path)]
+                                    )
+                                },
+                                lower::ControlCode::TapToggle(tt) => {
+                                    let tt_name = &tt.name;
+                                    let layer_ref_path = &tt.layer_ref.path;
+                                    let layer_ref_name = &tt.layer_ref.name;
+                                    let tap_key = &tt.tap_key;
+                                    quote!(
+                                        #key_path => vec![#tt_name(#layer_ref_path(#layer_ref_name.to_string()), #tap_key)]
+                                    )
+                                },
+                                lower::ControlCode::Exit(path) => {
+                                    quote!(
+                                        #key_path => vec![#path]
+                                    )
+                                },
+                            }
+                        })
+                        .collect()
+                })
+                .collect();
+            quote!(
+                qsk_types::Layer::from_hashmap(
+                    String::from(#name),
+                    hashmap!(
+                        #(#maps),*
+                    ),
+                    #active,
+                )
+            )
+        })
+        .collect();
+
+    quote!(
+        qsk_types::LayerComposer::from_layers(
+            vec![#(#layers_quoted),*]
+        )
+    )
 }
--- qsk-macros/src/lib.rs
+++ qsk-macros/src/lib.rs
@@ -11,17 +11,7 @@ mod parse;
 pub fn layer(ts: TokenStream) -> TokenStream {
     let ast = parse::parse(ts.clone().into());
     let model = analyze::analyze(ast);
-    let _ = lower::lower(model);
-    //let rust = codegen::codegen(ir).into();
-    TokenStream::new()
-}
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+    let ir = lower::lower(model);
+    let rust = codegen::codegen(ir).into();
+    rust
 }
