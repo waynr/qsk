8725cc1d5d0126492f8a3048d24f455dac70061c
Wayne Warren <wayne.warren.s@gmail.com>
2022-03-05 14:23:26 -0700

    qsk-macros: add basic crate-level usage documentation

--- qsk-macros/src/lib.rs
+++ qsk-macros/src/lib.rs
@@ -1,3 +1,31 @@
+//! # qsk-macros
+//!
+//! `qsk-macros` contains the `remap!` macro that enables `qsk` users to concisely define their
+//! keyboard remapping layers. Usage looks like the following:
+//!
+//! ```
+//! qsk_macros::remap!(
+//!     ModLayer[Active]: {
+//!         F -> TT(Navigation, F),
+//!     },
+//!     Navigation: {
+//!         END -> Exit(),
+//!         Y -> HOME,
+//!         U -> PAGEDOWN,
+//!         I -> PAGEUP,
+//!         O -> END,
+//!         H -> LEFT,
+//!         J -> DOWN,
+//!         K -> UP,
+//!         SEMICOLON -> RIGHT,
+//!     },
+//! )
+//! ```
+//!
+//! This mini keyboard-remapping DSL expands into a Rust expression with the type
+//! [`Result`](qsk_types::Result)<[`LayerComposer`](qsk_types::LayerComposer)>, which can is used
+//! in `qsk`'s remapping engine to actually perform keyboard transformations on input.
+//!
 use proc_macro::TokenStream;
 use proc_macro_error::proc_macro_error;
 
