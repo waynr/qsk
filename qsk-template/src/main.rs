c0571cba07b41d852f6a64a45699e0b213428189
Wayne Warren <wayne.warren.s@gmail.com>
2022-03-05 22:41:53 -0700

    qsk-template: get pre-template setup compiling

--- qsk-template/src/main.rs
+++ qsk-template/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::error;
+
+use qsk_macros;
+
+use qsk::entrypoint;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let layer_composer = qsk_macros::remap!(
+        ModLayer[Active]: {
+            Y -> HOME,
+            F -> TT(Navigation, F),
+        },
+        Navigation: {
+            END -> Exit(),
+            Y -> HOME,
+            U -> PAGEDOWN,
+            I -> PAGEUP,
+            O -> END,
+            H -> LEFT,
+            J -> DOWN,
+            K -> UP,
+            SEMICOLON -> RIGHT,
+        },
+    )?;
+
+    entrypoint(layer_composer)?;
+    Ok(())
 }
