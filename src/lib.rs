a55c59cb01a9ba9090041d669283d1916c49eb89
Wayne Warren <wayne.warren.s@gmail.com>
2022-03-05 20:53:37 -0700

    qsk: refactor main to use entrypoint function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 pub mod device;
+mod cli;
 pub mod engine;
 pub mod errors;
 pub mod events;
 pub mod layers;
 pub mod listener;
 pub mod recorder;
+
+mod entrypoint;
+
+pub use entrypoint::entrypoint;
